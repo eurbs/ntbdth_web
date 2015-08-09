d61e4d824af05f0e96cf8002f2a0fffa3254cd60
eurbs <emileeurbanek@gmail.com>
2015-08-09 13:09:04 -0700

    Added all storylines to csv. incorrect keys

--- app/views.py
+++ app/views.py
@@ -20,10 +20,11 @@ def answers():
     # if the key that was entered has a match in the first column of the
     # csv
     with open(os.path.join(PATH, 'app', 'answers.csv'), 'rt') as csvfile:
-        answerreader = csv.reader(csvfile, delimiter=',')
+        answerreader = csv.reader(csvfile, delimiter=';')
         for row in answerreader:
             if answerTry == row[0]:
-                return answerTry
+                # todo: figure out how to render an entry with multiple paragraphs
+                return row[1]
 
         # empty string if no answer was found
         return ""
